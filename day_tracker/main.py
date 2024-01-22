678d92d7eb2db5fca6ec1348647b0b68fe2a3739
Arslanodev <aamanov829@gmail.com>
2024-01-22 17:03:13 +0500

    Added some api endpoints for users and tasks

--- day_tracker/main.py
+++ day_tracker/main.py
@@ -1,72 +1,78 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from uuid import uuid4, UUID
-from day_tracker.models import Task
-from datetime import datetime
+import models
+import schemas
+import crud
 
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-running_task_database = {}
-database = {
-    "84fdad0b-2468-472b-be92-4f66bfb0239f": {
-        "task_id": "84fdad0b-2468-472b-be92-4f66bfb0239f",
-        "task_name": "Learning",
-        "spent_time": 0,
-    },
-    "d0b-2468-472b-be92-4f66bfb0239f": {
-        "task_id": "d0b-2468-472b-be92-4f66bfb0239f",
-        "task_name": "Reading",
-        "spent_time": 0,
-    },
-}
 
 
-@app.get("/api/v1/tracker")
-def get_task_names():
-    global database
-    if running_task_database:
-        return running_task_database
-    else:
-        return [value for key, value in database.items()]
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/api/v1/tracker/{task_id}")
-def get_details(task_id):
-    return database[task_id]
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@app.post("/api/v1/tracker")
-def add_new_task(task_data: Task):
-    id = uuid4()
-    task_data.task_id = id
-    database[id] = task_data
-
-    return task_data
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(db: Session = Depends(get_db)):
+    users = crud.get_users(db)
+    return users
 
 
-@app.delete("/api/v1/tracker/{task_id}")
-def delete_task(task_id):
-    pass
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
-@app.get("/api/v1/tracker/start/{task_id}")
+@app.post("/users/{user_id}/task/")
+def create_task_for_user(
+    user_id: int, task: schemas.TaskCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_task(db=db, task=task, user_id=user_id)
+
+
+@app.get("/users/{user_id}/task/")
+def get_tasks(user_id: int, db: Session = Depends(get_db)):
+    return crud.get_tasks(db, user_id)
+
+
+@app.get("users/{user_id}/task/{task_id}/start/")
 def start_tracker(task_id):
     global running_task_database
-    running_task_database = {"fkey": task_id, "created_date": datetime.now()}
+    running_task_database = {"fkey": task_id, "created_date": ""}
 
     return running_task_database
 
 
-@app.get("/api/v1/tracker/stop/")
+@app.get("users/{user_id}/task/{task_id}/stop/")
 def stop_tracker():
     global running_task_database
     date = running_task_database["created_date"]
-    current = datetime.now()
+    current = ""
     running_task_database = {}
 
     return date - current
 
 
-@app.get("/api/v1/tracker/{kind}")
+@app.get("/api/v1/task/{kind}")
 def generate_reports(kind):
     pass
